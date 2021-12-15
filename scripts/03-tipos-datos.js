bf004fd0f815491922ca8b24aa33d02ce8e45fc8
Roberto <mario24roberto@gmail.com>
2021-12-15 10:03:06 -0600

    Agregando Objetos literales y metodos para mostrar

---
+++ scripts/03-tipos-datos.js
@@ -0,0 +1,49 @@
+// String
+let texto = 'Roberto Ochaeta';
+
+// number
+let numero = 14;
+let decimal = 15.25;
+
+
+
+// Boolean 
+let mayor_Edad = true;
+let menor_Edad = Boolean(0);
+
+//Array
+let paises =['España','Mexico', 'Alemania']
+
+ 
+// null
+let vacio = null;
+// undefined
+let no_definido = undefined;
+
+console.log(paises[1]);
+console.log(typeof texto, typeof decimal,typeof numero,typeof mayor_Edad,typeof menor_Edad,typeof paises,
+    typeof vacio,
+    typeof no_definido);
+
+/* JSON
+    JavaScript Object Notation
+*/    
+
+let pelicula ={
+    titulo: 'EL Juego del calamar',
+    genero: 'Violencia',
+    year: 2021,
+    director: 'Robertio Ochaeta',
+    mostrar: function(){
+        return `
+        ***** La Pelicula de la semana *****
+         Titulo: ${this.titulo} 
+         Genero. ${this.genero} \n
+        Director: ${this.director}
+        Año: ${this.year}
+        `
+    }
+};
+console.log(pelicula.mostrar())
+console.log(pelicula.titulo, pelicula.genero);
+
